530fa9a9be831b76e818108d15ac357d9ef8592e
hive-r <iverzon051@gmail.com>
2025-10-21 02:28:28 +0800

    packages again

--- gimong/app/layout.tsx
+++ gimong/app/layout.tsx
@@ -7,7 +7,6 @@ import { Toaster } from "sonner";
 const robotoCondensed = Roboto_Condensed({
   subsets: ["latin"],
   weight: ["300", "400", "700"],
-  display: "swap",
 });
 
 export const metadata: Metadata = {
