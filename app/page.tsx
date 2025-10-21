239d7f6ea45112b9f4d122308d09880b87c218df
codexcancerion <melbertmarafo2022@gmail.com>
2025-10-21 21:49:15 +0800

    fix: add back homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-// import HomeModule from "@/modules/home/homeModule";
-// import Navbar from "@/components/layout/navbar"; 
-// import Footer from "@/components/layout/footer"; 
+import HomeModule from "@/modules/home/homeModule";
+import Navbar from "@/components/layout/navbar"; 
+import Footer from "@/components/layout/footer"; 
 
 export default function Page() {
   return (
     <>
-    TEST
-      {/* <Navbar />
+      <Navbar />
       <HomeModule />
-      <Footer /> */}
+      <Footer />
     </>
   );
 }
